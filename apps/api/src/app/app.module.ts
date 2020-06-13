636ac0328d1d58f6b175f652aa6a6dcc29cfd57d
Alfonso Andrés López Molina <osnoser_12@hotmail.com>
2020-06-12 22:10:03 -0500

    fix: ssr in nx

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { AngularUniversalModule } from "@nestjs/ng-universal";
 
+import { join } from 'path';
+
+import { AppServerModule } from "apps/nx-angular-ssr/src/app/app.server.module";
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    AngularUniversalModule.forRoot({
+      bootstrap: AppServerModule,
+      viewsPath: join(process.cwd(), 'dist/nx-angular-ssr/browser'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
